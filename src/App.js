2863955afc9d4d5de2057c150f18475688a21d22
SobhanBera <sobhanbera258@gmail.coms>
2020-12-19 18:04:46 +0530

    added main root

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import ErrorBoundary from './components/ErrorBoundary';
+import Minesweeper from './components/Minesweeper';
+import Animation from './components/Animation';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [theme, setTheme] = useState(false);
+	const [showAnim, setShowAnim] = useState(false);
+
+	const toggleAnimation = () => {
+		const curr = showAnim;
+		setShowAnim(!curr);
+	};
+
+	return (
+		<ErrorBoundary>
+			<div className={`main-app ${theme ? 'light' : 'dark'}`}>
+				<header>
+					<span
+						className={`${theme ? 'light' : 'dark'}`}
+						onClick={() => {
+							const curr = theme;
+							setTheme(!curr);
+						}}
+					>
+						Minesweeper
+					</span>
+				</header>
+				<main>
+					<Minesweeper
+						theme={theme ? 'light' : 'dark'}
+						toggleAnimation={toggleAnimation}
+					></Minesweeper>
+					{showAnim ? (
+						<Animation theme={theme ? 'light' : 'dark'} />
+					) : null}
+				</main>
+			</div>
+		</ErrorBoundary>
+	);
 }
 
 export default App;
